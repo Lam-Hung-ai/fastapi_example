03a915e23ec70618995ff1c2a1ceedd08ef28334
Lam-Hung-ai <2351260653@e.tlu.edu.vn>
2024-08-24 15:55:41 +0700

    delete file tmp.py

--- tmp.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime, timezone, timedelta
-ACCESS_TOKEN_EXPIRE_MINUTES= 30
-# Lấy thời gian hiện tại theo UTC
-current_time_utc = datetime.now(timezone.utc).astimezone(timezone(timedelta(hours=7)))
-expire = datetime.now(timezone.utc).astimezone(timezone(timedelta(hours=7))) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-
-print(current_time_utc)
-print(expire)
